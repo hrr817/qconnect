e319d23a282465a4a3d1df187a180d6e84098720
Hem Raj Rana <ryuu817@gmail.com>
2021-01-27 16:55:53 +0530

    initial commit

--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HelloWorld = () => {
-  return <div className="flx jus-ct al-ct content-max">Hello World</div>;
+  return <div className="flx jus-ct al-ct content-max">Qconnect</div>;
 };
 
 export default HelloWorld;
